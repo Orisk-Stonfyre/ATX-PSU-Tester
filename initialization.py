180f061e232cb56a43253f39a7efa2b7ca05de31
Orisk-Stonfyre <bpclark6p@outlook.com>
2025-01-30 14:35:44 -0500

    basic outline of init file/branch

---
+++ initialization.py
@@ -0,0 +1,11 @@
+#init spi and i2c
+
+#msg about waiting for init to complete before connecting psu
+
+# assert relays needed to meaure ground for refrence
+
+#read and save gnd reference values
+
+#deasert all relays
+
+#display connection msg
